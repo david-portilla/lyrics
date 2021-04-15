47fa21be2343fec28d33c1e9ffec1f884fec8a26
David Portilla <david_portilla@hotmail.com>
2021-04-15 13:10:07 -0500

    adding form layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Form from './components/Form';
 function App () {
   return (
     <>
-      Hi react app
       <Form></Form>
     </>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,51 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <h1>Form</h1>
+    <div className="bg-info">
+      <div className="container">
+        <div className="row">
+          <form className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+            <fieldset>
+              <legend className="text-center">Lyrics Finder</legend>
+
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label htmlFor="artist">Artist</label>
+                    <input
+                      type="text"
+                      name="artist"
+                      id="artist"
+                      className="form-control"
+                      placeholder="Artist name"
+                    />
+                  </div>
+
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label htmlFor="song">Song</label>
+                    <input
+                      type="text"
+                      name="song"
+                      id="song"
+                      className="form-control"
+                      placeholder="Song name"
+                    />
+                  </div>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary float-right"
+              >
+                Search
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
